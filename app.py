c2b7de7b5e9c2aff9f3332fba2d63319905b1991
Laura Roque <lroquecu@gmail.com>
2024-11-07 09:10:59 +0100

    very simple app file

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '''
+        <html>
+            <head>
+                <title>Simple Python App</title>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        text-align: center;
+                        margin-top: 50px;
+                        background-color: #f0f0f0;
+                    }
+                    h1 {
+                        color: #333;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>Hello, world from a very simple Python app!</h1>
+            </body>
+        </html>
+    '''
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
