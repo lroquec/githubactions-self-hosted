873fe783a3fa4076ceb0e316cbae6ec1eceda7ec
Laura Roque <lroquecu@gmail.com>
2024-11-07 15:01:31 +0100

    add tests

---
+++ tests/unit_test_app.py
@@ -0,0 +1,22 @@
+import unittest
+from app import app
+
+class TestFlaskApp(unittest.TestCase):
+
+    def setUp(self):
+        # Configure test client
+        app.testing = True
+        self.client = app.test_client()
+
+    def test_home_page(self):
+        # Send a GET request to path `/`
+        response = self.client.get('/')
+        
+        # Verify response 200 (OK)
+        self.assertEqual(response.status_code, 200)
+        
+        # Verify content in response
+        self.assertIn(b'Hello, world from a very simple Python app!', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
